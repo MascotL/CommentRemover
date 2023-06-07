9f7e8c901deb1ba9e71325bba5b6c73c1676e5d6
Wen Hanlin <2802420396@qq.com>
2023-06-07 14:37:27 +0800

    完善内容

--- CommentRemover.c
+++ CommentRemover.c
@@ -5,19 +5,27 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 
 void fileTypeFilter();
 void deleteAllFile();
 void delete(char *fileAddress);
+void delete_c(char *fileAddress);
+void delete_py(char *fileAddress);
+void delete_php(char *fileAddress);
+void delete_rb(char *fileAddress);
+void delete_html(char *fileAddress);
+void delete_css(char *fileAddress);
 
 int main() {
-    printf("These files will be executed:\n");
+    system("chcp 65001 > nul"); // 设置编码为UTF-8
+    printf("这些文件将被执行:\n");
 
     fileTypeFilter();
 
-    printf("\nEnter \"delete\" to execute\n");
-    printf("Or drag the file in.\n");
+    printf("\n输入 \"delete\" 执行\n");
+    printf("或者手动输入文件地址\n");
 
     while(1){
         printf("\n> ");
@@ -26,7 +34,7 @@ int main() {
         scanf("%s", &input);
 
         if (strcmp(input, "delete") == 0) { // 判断是否为delete
-            printf("Type 'y' to continue...\n\n");
+            printf("按 'y' 继续...\n\n");
             char *confirm;
 
             if(_getch() == 'y' || _getch() == 'Y') {
@@ -35,7 +43,7 @@ int main() {
         } else if (input[1] == ':' && input[2] == '\\') { // 判断是否为文件路径
             delete(input);
         } else { // 错误输入
-            printf("Invalid input.\n");
+            printf("请输入正确的路径\n");
         }
     }
 
@@ -50,7 +58,7 @@ void fileTypeFilter() {
     // 打开目录
     dir = opendir(directory);
     if (dir == NULL) {
-        printf("Cannot open directory.\n");
+        printf("无法打开当前文件夹\n");
         return;
     }
 
@@ -85,7 +93,7 @@ void deleteAllFile() {
     // 打开目录
     dir = opendir(directory);
     if (dir == NULL) {
-        printf("Cannot open directory.\n");
+        printf("无法打开当前文件夹\n");
         return;
     }
 
@@ -116,23 +124,159 @@ void deleteAllFile() {
                 strcmp(extension, ".html") == 0 ||
                 strcmp(extension, ".css") == 0) {
                 // 将文件路径拼接
-                printf("\"%s\" Deletion...\n", entry->d_name);
-                strcpy(fileAddress, buffer);
-                char *lastZero = strrchr(fileAddress, '\0');
-                strcpy(lastZero, entry->d_name);
-                delete(fileAddress);
+                printf("\"%s\" 删除中...\n", entry->d_name); // 打印文件名
+                strcpy(fileAddress, buffer); // 拼接文件夹路径
+                char *lastZero = strrchr(fileAddress, '\0'); // 获取字符串结束符的位置
+                strcpy(lastZero, entry->d_name); // 拼接文件名
+                delete(fileAddress); // 删除文件
             }
         }
     }
-    printf("Done!\n");
+    printf("完成!\n");
     closedir(dir);
 }
 
 void delete(char *fileAddress) {
     if(access(fileAddress, F_OK) != 0) {
-        printf("\"%s\" does not exist!", fileAddress);
+        printf("\"%s\" 路径不存在", fileAddress);
         return;
     }
-    /* delete().c */
+    // 判断文件名后缀
+    const char *extension = strrchr(fileAddress, '.');
+    if (*extension == NULL) {
+        printf("\"%s\" 未知文件", fileAddress);
+        return;
+    } else if ( *extension == ".c" ||
+                *extension == ".cpp" ||
+                *extension == ".java" ||
+                *extension == ".js" ||
+                *extension == ".swift" ||
+                *extension == ".rs" ||
+                *extension == ".go" ) {
+        delete_c(fileAddress);
+    } else if (*extension == ".py") {
+        delete_py(fileAddress);
+    } else if (*extension == ".php") {
+        delete_php(fileAddress);
+    } else if (*extension == ".rb") {
+        delete_rb(fileAddress);
+    } else if (*extension == ".html") {
+        delete_html(fileAddress);
+    } else if (*extension == ".css") {
+        delete_css(fileAddress);
+    } else {
+        printf("\"%s\" 非法文件", fileAddress);
+        return;
+    }
 }
 
+void delete_c(char *fileAddress){
+    // 获取文件名后缀
+    const char *_extension = strrchr(fileAddress, '.');
+
+    // 打开文件
+    FILE *originalFile, *tempFile;
+    char originalFileName[] = fileAddress;
+    char tempFileName[] = "temp" + *_extension;
+    
+    // 以只读方式打开原文件，以写入方式打开临时文件
+    originalFile = fopen(originalFileName, "r");
+    tempFile = fopen(tempFileName, "w");
+    
+    // 检查文件是否打开成功
+    if  (originalFile == NULL || tempFile == NULL) {
+        printf("cannot open\n");
+        return 1;
+    }
+    
+    char line[256] = ""; // 用于存储读取的行
+    bool isComment = false;
+    
+    // 读取一行到line[]，直到文件结尾
+    while (fgets(line, sizeof(line), originalFile) != NULL) {
+        char *ss = strstr(line, "//");
+        char *sa = strstr(line, "/*");
+        char *as = strstr(line, "*/");
+        
+        if (isComment){
+            if (as == NULL) {       // "*/"不存在
+                memset(line, 0, sizeof(line));
+                continue;
+            } else {        // "*/"存在
+                for (int i = 0; ; i++) {
+                    line[i] = ' ';
+                    if (&line[i]-1 == as) {
+                        fputs(line, tempFile);
+                        isComment = false;
+                        break;
+                    }
+                }
+            }
+        } else if (ss == NULL && sa == NULL) { // 无注释
+            fputs(line, tempFile);
+        } else if (ss != NULL && sa == NULL) { // "//"存在
+            *ss = '\n';
+            *(ss + 1) = '\0';
+            fputs(line, tempFile);
+        } else if (ss == NULL && sa != NULL) { // "/*"存在
+            if (as != NULL) {       // "*/"也存在
+                for (int i = 0; ; i++) {
+                    *(sa + i) = ' ';
+                    if ((sa+i-1) == as) {
+                        fputs(line, tempFile);
+                        break;
+                    }
+                }
+            } else {        // "*/"不存在
+                isComment = true;
+                *sa = '\n';
+                *(sa + 1) = '\0';
+                fputs(line, tempFile);
+            }
+        } else if (ss != NULL && sa != NULL) { // "//"和"/*"都存在
+            if (ss < sa) {
+                *ss = '\n';
+                *(ss + 1) = '\0';
+                fputs(line, tempFile);
+            } else {
+                isComment = true;
+                *sa = '\n';
+                *(sa + 1) = '\0';
+                fputs(line, tempFile);
+            }
+        }
+        memset(line, 0, sizeof(line)); // 清空line[]
+    }
+    
+    // 关闭文件
+    fclose(originalFile);
+    fclose(tempFile);
+
+    printf("\n执行完成\n");
+    
+    // 删除原文件，将临时文件重命名为原文件
+    remove(originalFileName);
+    rename(tempFileName, originalFileName);
+    
+    return 0;
+}
+
+void delete_py(char *fileAddress){
+    printf("开发中...\n");
+}
+
+void delete_php(char *fileAddress){
+    printf("开发中...\n");
+}
+
+void delete_rb(char *fileAddress){
+    printf("开发中...\n");
+}
+
+void delete_html(char *fileAddress){
+    printf("开发中...\n");
+}
+
+void delete_css(char *fileAddress){
+    printf("开发中...\n");
+}
